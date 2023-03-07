c18c03e161ac5e82772c1d5eacedbb6aee02eab1
Scott Lett <slett@holisticmath.com>
2023-03-07 11:06:42 -0700

    lots of experimentation

--- src/read_wave.sh
+++ src/read_wave.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 cd /home/pi/projects/wave-reader/src
-nohup sudo python read_wave2.py --SERIAL_NUMBER=2950044984  --LOGGING=INFO --SAMPLE_PERIOD=600 &
+nohup python read_wave2.py --SERIAL_NUMBER=2950044984  --LOGGING=INFO --SAMPLE_PERIOD=600 &
